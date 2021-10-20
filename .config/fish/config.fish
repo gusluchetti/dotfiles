362b0c22cc409ca5bed4e29c7deb0e2c93593a5b
Gustavo Luchetti <gusta.luchetti@gmail.com>
2021-10-20 15:03:55 -0300

    more configs!

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,7 @@
 fish_ssh_agent
-alias ls="exa -l -a"
+set PATH (string match -v '/mnt/c/Program Files/Graphviz/bin"' $PATH)
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/rachel-ubuntu/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
